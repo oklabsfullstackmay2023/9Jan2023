3f875455260f4fc64201e61c2dc15904a3e50fe2
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2024-01-09 12:38:06 +0530

    'function difference between'

---
+++ function.py
@@ -0,0 +1,7 @@
+#1. Function defination is one time process
+def myFunction(): #function name always written in camelCase
+    print(f'hello for vishal')
+    pass
+
+#2. Function calling/invoking is many time process
+myFunction()
